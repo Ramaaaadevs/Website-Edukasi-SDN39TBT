293872256e1a17ec3603934b85eba1b7c6413fd0
Ramaaaadevs <diwan.123140116@student.itera.ac.id>
2026-01-21 19:02:12 +0700

    membuat default page kosong

---
+++ src/app/evaluasi/[mapelSlug]/page.jsx
@@ -0,0 +1,8 @@
+export default function mapelSlug() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center p-24">
+      <h1 className="text-4xl font-bold mb-4">Halaman Result</h1>
+      <p className="text-xl">Halaman hasil ujian belum tersedia.</p>
+    </div>
+  );
+}
